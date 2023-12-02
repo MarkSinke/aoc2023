e94c27ffb2f923e21e6e6dbf303edc660fe5365e
Mark Sinke <MarkSinke@users.noreply.github.com>
2023-12-02 15:01:10 +0100

    Day 1 star 2 naive attempt. Snag: for example "twone" is replaced by tw1, which is correct late in a string, but not early, and "oneight" is replaced by 1ight which is correct early, but not late

--- day1_test.go
+++ day1_test.go
@@ -2,6 +2,7 @@ package aoc2023
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -25,5 +26,29 @@ func ComputeCalibrationSum(t *testing.T, filename string) int {
 }
 
 func TestDay1Input(t *testing.T) {
-	fmt.Printf("Day 1 result: %d\n", ComputeCalibrationSum(t, "day1input.txt"))
+	fmt.Printf("Day 1 result (star 1): %d\n", ComputeCalibrationSum(t, "day1input.txt"))
+}
+
+func ComputeCalibrationSumWordAware(t *testing.T, filename string) int {
+	lines, err := ReadFile(filename)
+	if err != nil {
+		t.Fatal("input file cannot be read")
+	}
+	sum := 0
+	replacer := strings.NewReplacer("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9")
+	for _, str := range lines {
+		sum += CalibrationValue(FirstAndLastDigit(replacer.Replace(str)))
+	}
+	return sum
+}
+
+func TestDay1Example2(t *testing.T) {
+	sum := ComputeCalibrationSumWordAware(t, "day1input_example2.txt")
+	if sum != 281 {
+		t.Errorf("expected 142, but got %d", sum)
+	}
+}
+
+func TestDay1InputStar2(t *testing.T) {
+	fmt.Printf("Day 1 result (star 2): %d\n", ComputeCalibrationSumWordAware(t, "day1input.txt"))
 }
