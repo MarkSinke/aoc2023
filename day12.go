3977d2a0fbd758b9f22ddee491b61a4cbc3de8f5
Mark Sinke <MarkSinke@users.noreply.github.com>
2023-12-12 12:11:59 +0100

    Day 12 star 2 (too slow)

--- day12.go
+++ day12.go
@@ -26,29 +26,80 @@ func (r SpringRecord) Matches() bool {
 }
 
 func (r SpringRecord) PossibleMatches() int {
+	// fmt.Println("matching", r)
 	before, after, found := strings.Cut(r.record, "?")
 	if !found {
+		// fmt.Println("  !found", r.Matches())
 		if r.Matches() {
 			return 1
 		} else {
 			return 0
 		}
 	}
+	options := strings.Count(after, "?") + strings.Count(after, "#")
 	runs := runRegexp.FindAllString(before, -1)
+	// fmt.Println("  runs", runs)
+	if len(runs) == 0 {
+		return SpringRecord{after, r.counts}.PossibleMatches() + SpringRecord{"#" + after, r.counts}.PossibleMatches()
+	}
 	if len(runs) > len(r.counts) {
 		return 0
 	}
-	for i, run := range runs {
-		// a short run might still turn into a longer one, but a mismatched too-long run cannot be repaired
-		if len(run) > r.counts[i] {
+	// very rough upper bound to avoid lots of recursion for long count lists
+	if len(r.counts)-len(runs)-1 > options {
+		// fmt.Println("shortcut", r, before, after, runs, options)
+		return 0
+	}
+
+	for i := 0; i < len(runs)-1; i++ {
+		if len(runs[i]) != r.counts[i] {
 			return 0
 		}
 	}
+	lastIndex := len(runs) - 1
+	lastRun := runs[lastIndex]
+	// fmt.Println("  lI, lR", lastIndex, lastRun)
+	return r.possibleMatchesDot(lastIndex, lastRun, after) + r.possibleMatchesHash(before, lastIndex, lastRun, after)
+}
 
-	resDot := SpringRecord{before + "." + after, r.counts}.PossibleMatches()
-	resHash := SpringRecord{before + "#" + after, r.counts}.PossibleMatches()
+func (r SpringRecord) possibleMatchesDot(lastIndex int, lastRun string, after string) int {
+	if len(lastRun) != r.counts[lastIndex] {
+		return 0
+	}
 
-	return resDot + resHash
+	rec := SpringRecord{after, r.counts[lastIndex+1:]}
+	// fmt.Println("recurse .", rec)
+	return rec.PossibleMatches()
+}
+
+func (r SpringRecord) possibleMatchesHash(before string, lastIndex int, lastRun string, after string) int {
+	if before[len(before)-1] == '#' {
+		if len(lastRun) > r.counts[lastIndex] {
+			// a short run might still turn into a longer one, but a mismatched too-long run cannot be repaired
+			return 0
+		}
+
+		rec := SpringRecord{lastRun + "#" + after, r.counts[lastIndex:]}
+		// fmt.Println("recurse #1", rec)
+		return rec.PossibleMatches()
+	} else {
+		if len(lastRun) != r.counts[lastIndex] {
+			return 0
+		}
+
+		rec := SpringRecord{"#" + after, r.counts[lastIndex+1:]}
+		// fmt.Println("recurse #2", rec)
+		return rec.PossibleMatches()
+	}
+}
+
+func (r SpringRecord) Unfold() SpringRecord {
+	newRecord := r.record + "?" + r.record + "?" + r.record + "?" + r.record + "?" + r.record
+	newCounts := append(r.counts, r.counts...)
+	newCounts = append(newCounts, r.counts...)
+	newCounts = append(newCounts, r.counts...)
+	newCounts = append(newCounts, r.counts...)
+	return SpringRecord{newRecord, newCounts}
 }
 
 func ReadSpringRecords(path string) []SpringRecord {
--- day12_test.go
+++ day12_test.go
@@ -32,8 +32,50 @@ func TestDay12Input(t *testing.T) {
 
 	sum := 0
 	for _, record := range records {
-		sum = sum + record.PossibleMatches()
+		matches := record.PossibleMatches()
+		sum = sum + matches
 	}
 
 	fmt.Println("Day 12 result (star 1):", sum)
+}
+
+func TestDay12Unfold(t *testing.T) {
+	record := SpringRecord{".#", []int{1}}
+
+	unfolded := record.Unfold()
+
+	assert.Equal(t, ".#?.#?.#?.#?.#", unfolded.record)
+	assert.Equal(t, []int{1, 1, 1, 1, 1}, unfolded.counts)
+}
+
+func TestDay12Star2InputExample(t *testing.T) {
+	records := ReadSpringRecords("day12input_example.txt")
+
+	sum := 0
+	for _, record := range records {
+		matches := record.Unfold().PossibleMatches()
+		sum = sum + matches
+	}
+
+	assert.Equal(t, 525152, sum)
+}
+
+func XTestDay12Star2Input(t *testing.T) {
+	records := ReadSpringRecords("day12input.txt")
+
+	sum := 0
+	for _, record := range records {
+		fmt.Print("matching", record)
+		matches := record.Unfold().PossibleMatches()
+		fmt.Println(" ->", matches)
+		sum = sum + matches
+	}
+
+	fmt.Println("Day 12 result (star 2):", sum)
+}
+
+func TestDay12Star2HardRecord(t *testing.T) {
+	record := SpringRecord{"..?????.????", []int{1, 1, 1, 2}}
+
+	assert.Equal(t, 0, record.Unfold().PossibleMatches())
 }
