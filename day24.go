e1eceda85d0ea69bf0062fa2c85c24c05d416661
Mark Sinke <MarkSinke@users.noreply.github.com>
2023-12-24 22:56:16 +0100

    Day 24 star 2 closing in, still floating point errors

--- day24.go
+++ day24.go
@@ -13,6 +13,10 @@ type Coord3F struct {
 	z float64
 }
 
+func (c Coord3F) ToDir() Dir3F {
+	return Dir3F{c.x, c.y, c.z}
+}
+
 type Dir3F struct {
 	dx float64
 	dy float64
@@ -29,6 +33,14 @@ func (d Dir3F) Times(f float64) Dir3F {
 
 func (d Dir3F) DivideBy(f float64) Dir3F {
 	return Dir3F{d.dx / f, d.dy / f, d.dz / f}
+}
+
+func (d0 Dir3F) Plus(d1 Dir3F) Dir3F {
+	return Dir3F{d0.dx + d1.dx, d0.dy + d1.dy, d0.dz + d1.dz}
+}
+
+func (d0 Dir3F) Minus(d1 Dir3F) Dir3F {
+	return Dir3F{d0.dx - d1.dx, d0.dy - d1.dy, d0.dz - d1.dz}
 }
 
 func (c Coord3F) Plus(d Dir3F) Coord3F {
@@ -82,7 +94,7 @@ func parseHail(str string) HailStone {
 	return HailStone{pos, dir}
 }
 
-func intersectXY(h1 HailStone, h2 HailStone) *Coord2F {
+func intersectXY(h1 HailStone, h2 HailStone) (float64, float64, *Coord2F) {
 	// intersect the line h1 and h2 - for some t1 ant t2 (float):
 	// (h1.pos.x + h1.dir.dx * t1, h1.pos.y + h1.dir.dy * t1) = (h2.pos.x + h2.dir.dx * t2, h2.pos.y + h2.dir.dy * t2)
 	// i.e.,
@@ -109,15 +121,15 @@ func intersectXY(h1 HailStone, h2 HailStone) *Coord2F {
 	denom := h2.dir.dx*h1.dir.dy - h2.dir.dy*h1.dir.dx
 	if denom == 0 {
 		// parallel
-		return nil
+		return 0, 0, nil
 	}
 	t2 := ((h2.pos.y-h1.pos.y)*h1.dir.dx - (h2.pos.x-h1.pos.x)*h1.dir.dy) / denom
 	t1 := (h2.pos.x - h1.pos.x + h2.dir.dx*t2) / h1.dir.dx
 	if t1 < 0.0 || t2 < 0.0 {
 		// crossed in the past
-		return nil
+		return 0, 0, nil
 	}
-	return &Coord2F{float64(h2.pos.x) + t2*float64(h2.dir.dx), float64(h2.pos.y) + t2*float64(h2.dir.dy)}
+	return t1, t2, &Coord2F{float64(h2.pos.x) + t2*float64(h2.dir.dx), float64(h2.pos.y) + t2*float64(h2.dir.dy)}
 }
 
 func CountIntersectionPairs(hails []HailStone, min float64, max float64) int {
@@ -128,7 +140,7 @@ func CountIntersectionPairs(hails []HailStone, min float64, max float64) int {
 			h1 := hails[i]
 			h2 := hails[j]
 
-			c := intersectXY(h1, h2)
+			_, _, c := intersectXY(h1, h2)
 			if c != nil && c.x >= min && c.x <= max && c.y >= min && c.y <= max {
 				count++
 			}
@@ -136,6 +148,28 @@ func CountIntersectionPairs(hails []HailStone, min float64, max float64) int {
 	}
 
 	return count
+}
+
+func FindIntersectionPairsXY(hails []HailStone, min float64, max float64) (HailStone, HailStone, HailStone, HailStone) {
+	found := []HailStone{}
+
+	for i := range hails {
+		for j := i + 1; j < len(hails); j++ {
+			h1 := hails[i]
+			h2 := hails[j]
+
+			_, _, c := intersectXY(h1, h2)
+			if c != nil && c.x >= min && c.x <= max && c.y >= min && c.y <= max {
+				found = append(found, h1, h2)
+			}
+
+			if len(found) == 4 {
+				return found[0], found[1], found[2], found[3]
+			}
+		}
+	}
+
+	panic("need two intersecting pairs in XY")
 }
 
 func crossProduct(d0, d1 Dir3F) Dir3F {
@@ -193,118 +227,52 @@ func intersectPlaneAndLine(p0 Coord3F, n Dir3F, h HailStone) (Coord3F, float64) 
 	return p, t
 }
 
-func ComputeIntersectingHailStone(hails []HailStone) HailStone {
-	// if the new stone h intersects with h0, h1, h2, ... it intersects with each of the paths:
-	// FORALL i in N : h.pos + h.dir * t_i = h_i.pos + t_i * h_i.dir
-	// This means it needs to hit the first two hailstones in the plane defined by their paths (i.e.,
-	// the plane that goes through both paths). To make sure we have a plane P that holds both lines,
-	// we look for a pair where two paths are parallel, say h0 and h1.
-	// A 3rd hail stone h2 then hits this plane P somewhere and that
-	// is where h intersects with h2. We can compute that intersection point and timestamp.
-	// If we do the same for another hail stone h3, we have two positions and timestamps,
-	// and hence we can compute h.pos and h.dir.
-	h0, h1 := findParallelPair(hails)
-	fmt.Println("h0", h0, "h1", h1)
-	n := crossProduct(h0.dir, h1.pos.MinusToDir(h0.pos)) // normal of the intersection plane
-	//n = n.DivideBy(n.Length())
+func ComputeIntersectingHailStone(hails []HailStone, min, max float64) HailStone {
+	// translate everything into a system with hails[0] as the frame of reference
+	h0shift := hails[0].pos.ToDir()
+	h0dir := hails[0].dir
+	hailsR := make([]HailStone, len(hails))
+	for i, h := range hails {
+		hailsR[i] = HailStone{h.pos.Minus(h0shift), h.dir.Minus(h0dir)}
+	}
+
+	// in hailsR, the first hail stone is at (0,0,0) and does not move, so the rock needs
+	// to pass through (0,0,0)
+	// hailsR[1] follows a trajectory that the rock needs to intersect with. This means that
+	// it intersects somewhere in the plane defined by the origin and any 2 points on that line
+	// we can compute the unit vector of that plane by taking the vectors from the origin to t=0 and t=1
+	// after that, we compute two intersections for hailsR[2] and hailsR[3] and work out dir and p0 from
+	// there
+	h1 := hailsR[1]
+	p10 := h1.pos
+	p11 := h1.pos.Plus(h1.dir)
+	n := crossProduct(p10.ToDir(), p11.ToDir())
+	fmt.Println("h0", hailsR[0], "h1", h1)
+	fmt.Println("p10", p10, "p11", p11)
 	fmt.Println("n", n)
-	p0 := h0.pos // a point in the intersection plane
-	fmt.Println("p0", p0)
-	h2, h3 := findTwoOtherStones(h0, h1, n, hails)
+
+	h2 := hailsR[2]
+	h3 := hailsR[3]
 	fmt.Println("h2", h2, "h3", h3)
 
-	fmt.Println("dp2", dotProduct(h2.dir, n))
-	fmt.Println("dp3", dotProduct(h3.dir, n))
+	p0 := Coord3F{0, 0, 0}
 	p2, t2 := intersectPlaneAndLine(p0, n, h2)
 	fmt.Println("p2", p2, "t2", t2)
 	p3, t3 := intersectPlaneAndLine(p0, n, h3)
 	fmt.Println("p3", p3, "t3", t3)
 
-	// direction vector travels from p2 to p3 in t2-t3 time
-	dir := (p2.MinusToDir(p3)).DivideBy(t2 - t3)
-	fmt.Println("dir", dir)
-	p := p2.Minus(dir.Times(t2))
-	fmt.Println("p", p)
+	fmt.Println("t2-t3", t2-t3)
+	dir := p2.MinusToDir(p3).DivideBy(t2 - t3)
+	fmt.Println("dir before round", dir)
+	fmt.Println("t2 diff", t2-math.Round(t2))
+	fmt.Println("t3 diff", t3-math.Round(t3))
+	// fix some floating point errors by rounding
+	dir = Dir3F{math.Round(dir.dx), math.Round(dir.dy), math.Round(dir.dz)}
+	//	t2 = math.Round(t2)
+	pos := p2.Minus(dir.Times(t2))
+	fmt.Println("dir", dir, "pos", pos)
 
-	fmt.Println("intersect h2", h2.pos.Plus(h2.dir.Times(t2)), p.Plus(dir.Times(t2)))
-	fmt.Println("intersect h3", h3.pos.Plus(h3.dir.Times(t3)), p.Plus(dir.Times(t3)))
-
-	return HailStone{p, dir}
-	//
-	// In this equations, h.pos (3 unknowns), h.dir (3 unknowns), and FORALL i: t_i are unknown
-	// each of the above equations effectively turns into 3 equations
-	// so we have 3N equations with 6 + N unknowns. This is solvable for N >= 3.
-	// solving for h.pos and h.dir hence involves solving a system of equations of size at least 3N=21.
-
-	// h.pos + h.dir * t0 = h0.pos + h0.dir * t0
-	// h.pos + h.dir * t1 = h1.pos + h1.dir * t1
-	// h.pos + h.dir * t2 = h2.pos + h2.dir * t2
-	//
-	// writing out the equations yields:
-	// h.pos.x + h.dir.dx * t0 = h0.pos.x + h0.dir.dx * t0
-	// h.pos.y + h.dir.dy * t0 = h0.pos.y + h0.dir.dy * t0
-	// h.pos.z + h.dir.dz * t0 = h0.pos.z + h0.dir.dz * t0
-	//
-	// h.pos.x + h.dir.dx * t1 = h1.pos.x + h1.dir.dx * t1
-	// h.pos.y + h.dir.dy * t1 = h1.pos.y + h1.dir.dy * t1
-	// h.pos.z + h.dir.dz * t1 = h1.pos.z + h1.dir.dz * t1
-	//
-	// h.pos.x + h.dir.dx * t2 = h2.pos.x + h2.dir.dx * t2
-	// h.pos.y + h.dir.dy * t2 = h2.pos.y + h2.dir.dy * t2
-	// h.pos.z + h.dir.dz * t2 = h2.pos.z + h2.dir.dz * t2
-	//
-	// Or,
-	// (h.dir.dx - h0.dir.dx) * t0 = h0.pos.x - h.pos.x
-	// (h.dir.dy - h0.dir.dy) * t0 = h0.pos.y - h.pos.y
-	// (h.dir.dz - h0.dir.dz) * t0 = h0.pos.z - h.pos.z
-	// [...]
-	// ->
-	// t0 = (h0.pos.x - h.pos.x) / (h.dir.dx - h0.dir.dx)
-	// t0 = (h0.pos.y - h.pos.y) / (h.dir.dy - h0.dir.dy)
-	// t0 = (h0.pos.z - h.pos.z) / (h.dir.dz - h0.dir.dz)
-	// [...]
-	// -> (eliminate t0, t1, t2)
-	// (h0.pos.x - h.pos.x) / (h.dir.dx - h0.dir.dx) = (h0.pos.y - h.pos.y) / (h.dir.dy - h0.dir.dy)
-	// (h0.pos.y - h.pos.y) / (h.dir.dy - h0.dir.dy) = (h0.pos.z - h.pos.z) / (h.dir.dz - h0.dir.dz)
-	// (h0.pos.z - h.pos.z) / (h.dir.dz - h0.dir.dz) = (h0.pos.x - h.pos.x) / (h.dir.dx - h0.dir.dx)
-	// [...]
-	// -> (multiply both sides to get rid of denominators)
-	// (h0.pos.x - h.pos.x) * (h.dir.dy - h0.dir.dy) = (h0.pos.y - h.pos.y) * (h.dir.dx - h0.dir.dx)
-	// (h0.pos.y - h.pos.y) * (h.dir.dz - h0.dir.dz) = (h0.pos.z - h.pos.z) * (h.dir.dy - h0.dir.dy)
-	// (h0.pos.z - h.pos.z) * (h.dir.dx - h0.dir.dx) = (h0.pos.x - h.pos.x) * (h.dir.dz - h0.dir.dz)
-	// [...]
-	// h0.pos.x * h.dir.dy - h0.pos.x * h0.dir.dy - h.pos.x * h.dir.dy + h.pos.x * h0.dir.dy
-	//
-	// Consider these equations in a Newtonian system S moving with constant velocity h.dir.
-	// Then we are looking for the first 3 hail stone paths to travel through a single point h.pos.
-	// I.e. the intersection point of the first 3 lines in S. From there we can compute back.
-	// h.pos = (h0.dir - h.dir) * t0 + h0.pos
-	// h.pos = (h1.dir - h.dir) * t1 + h1.pos
-	// h.pos = (h2.dir - h.dir) * t2 + h2.pos
-
-	// We can compute the intersection point h.pos from the first 2 lines.
-	// Consider the plane rooted at h0.pos, extending in all directions orthogonal to h0.dir. Similarly,
-	// for h1, and h2. Then the intersection point P is the point where
-	// d0' . h0.dir = 0
-	// h0.pos + d0' = P
-	// d1' . h1.dir = 0
-	// h1.pos + d1' = P
-	// d2' . h2.dir = 0
-	// h2.pos + d2' = P =>
-	//
-	// d0'.dx * h0.dir.dx + d0'.dy * h0.dir.dy + d2' * h0.dir.dz = 0
-	// h0.pos.x + d0'.dx = P.x
-	// h0.pos.y + d0'.dy = P.y
-	// h0.pos.z + d0'.dz = P.z
-	//
-	// d1'.dx * h1.dir.dx + d1'.dy * h1.dir.dy + d2' * h1.dir.dz = 0
-	// h1.pos.x + d1'.dx = P.x
-	// h1.pos.y + d1'.dy = P.y
-	// h1.pos.z + d1'.dz = P.z
-	//
-	// d2'.dx * h2.dir.dx + d2'.dy * h2.dir.dy + d2' * h2.dir.dz = 0
-	// h2.pos.x + d2'.dx = P.x
-	// h2.pos.y + d2'.dy = P.y
-	// h2.pos.z + d2'.dz = P.z
-
+	res := HailStone{pos.Plus(h0shift), dir.Plus(h0dir)}
+	fmt.Println("res", res)
+	return res
 }
--- day24_test.go
+++ day24_test.go
@@ -2,6 +2,7 @@ package aoc2023
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,7 +27,7 @@ func TestDay24Input(t *testing.T) {
 func TestDay24Star2InputExample(t *testing.T) {
 	hails := ReadHail("day24input_example.txt")
 
-	h := ComputeIntersectingHailStone(hails)
+	h := ComputeIntersectingHailStone(hails, 7, 27)
 
 	assert.Equal(t, Coord3F{24, 13, 10}, h.pos)
 	assert.Equal(t, Dir3F{-3, 1, 2}, h.dir)
@@ -35,10 +36,10 @@ func TestDay24Star2InputExample(t *testing.T) {
 func TestDay24Star2Input(t *testing.T) {
 	hails := ReadHail("day24input.txt")
 
-	h := ComputeIntersectingHailStone(hails)
+	h := ComputeIntersectingHailStone(hails, 2e14, 4e14)
 
 	fmt.Println("h", h)
-	fmt.Println("Day 24 result (star 2):", h.pos.x+h.pos.y+h.pos.z)
+	fmt.Println("Day 24 result (star 2):", int(math.Round(h.pos.x+h.pos.y+h.pos.z)))
 	assert.Equal(t, Coord3F{24, 13, 10}, h.pos)
 	assert.Equal(t, Dir3F{-3, 1, 2}, h.dir)
 }
